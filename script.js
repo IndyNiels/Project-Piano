2c0ab46efeab1ed8c07c315e320798d5dd477794
Indy Higuet <93846160+IndyNiels@users.noreply.github.com>
2022-09-01 16:03:00 +0000

    Several updates.

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ class Note {
     }
 }
 
-
 const f6 = new Note('f6', document.getElementById("pos22"), 'treble', 3.5)
 const e6 = new Note('e6', document.getElementById("pos21"), 'treble', 3)
 const d6 = new Note('d6', document.getElementById("pos20"), 'treble', 2.5)
@@ -60,43 +59,55 @@ const b1 = new Note('b1', document.getElementById("pos2"), 'bass')
 const a1 = new Note('a1', document.getElementById("pos1"), 'bass')
 
 
+let notas = [f6, e6, d6, c6, b5, a5, g5, f5, e5, d5, c5, b4, a4, g4, f4, e4, d4, c4, b3, a3, g3, f3, a4b, g4b, e4b, b3b, a3b, g3b,f3b, e3, d3, c3, b2, a2, g2, f2, e2, d2, c2, b1, a1 ];
 
 
-let notas = [f6, e6, d6, c6, b5, a5, g5, f5, e5, d5, c5, b4, a4, g4, f4, e4, d4, c4, b3, a3, g3, f3, a4b, g4b, e4b, b3b, a3b, g3b,
-    f3b, e3, d3, c3, b2, a2, g2, f2, e2, d2, c2, b1, a1 ];
+let mySong = [c3,g3,e4,f2,a3,c2,g4,e5,c4,a4,d3,b3,b4,b1,b2,f3,f4,a1,g2,a2,e3,d2,e2,c5,d4,d5,g5]
 
 //Adding to timer 100 seconds to all notes
-notas.map(x => x.timer = 100)
+const addTimer = (notes) => notes.map(x=> x.timer = 100);
 
 //Adding position to notes from left to right of the keyboard starting from 0 
-notas.reverse()
-for (let i = 0; i<notas.length; i++) {
-  notas[i].position = i
-}
+const addPosition = (notes) => {
+  notes.reverse()
+for (let i = 0; i<notes.length; i++) {
+  notes[i].position = i
+}};
 
 //Sort for initializing game. We start from c4 (23 position)
-notas.sort((a,b) => (Math.abs(a.position-29)> Math.abs(b.position -29))? 1 : -1)
-
-//console.table(notas)
+const sortNotes = (notes) => notes.sort((a,b) => (Math.abs(a.position-29)> Math.abs(b.position -29))? 1 : -1)
 
 
+//Loading notes to play (Depending on the song)
 
 
-//Declaring the visible note.
+const initialize = (notes) => {
+  addTimer(notes);
+  addPosition(notes);
+  sortNotes(notes);
+  dibuja(notes);
+};
 
-let notaVisible = []
+//Saving the progress when we close the window
+window.addEventListener('beforeunload', function () {
+  localStorage.setItem('savedSong', JSON.stringify(mySong));
+});
+
+// At loading event -> Initiate saved item, if doesns exist state explicitly. 
+
+
+
+
+
 
 //Here I declare and get my buttons. 
-
 let boton = document.querySelector('#start-button');
 
 // Declaring my clefs. 
-
 let treble = document.querySelector('.gclef');
 let bass = document.querySelector('.bass');
 
-//Declaring my lines .
-
+//Declaring my lines
 let line1up = document.querySelector('.line1-up');
 let line2up = document.querySelector('.line2-up');
 let line3up = document.querySelector('.line3-up');
@@ -104,47 +115,24 @@ let line3up = document.querySelector('.line3-up');
 
 //My functions ==================================================================
 
-//Declaring my animation confeti 
-
-   const svgContainer = document.getElementById('svg');
-        const animItem = bodymovin.loadAnimation({
-            wrapper: svgContainer,
-            animType: 'svg',
-            loop: false,
-            autoplay: false,
-            path: 'https://assets2.lottiefiles.com/packages/lf20_u4yrau.json'
-        });
-
-       
-                                        
-         let confeti = function (){
-            svgContainer.classList.remove('hide');
-            animItem.goToAndPlay(0,true);
-        }
-
-     boton.addEventListener('click',confeti) 
-
-        animItem.addEventListener('complete', () => {
-            svgContainer.classList.add('hide');
-            
-        })  
-
 
 //Declaring my sorting algorithm
 
-let algo = function () {
-notas.sort((a, b) => (a.timer > b.timer) ? 1 : (a.timer === b.timer) ? ((Math.abs(a.position-23) > Math.abs(b.position-23)) ? 1 : -1) : -1 ); 
+let algo = function (notes) {
+notes.sort((a, b) => (a.timer > b.timer) ? 1 : (a.timer === b.timer) ? ((Math.abs(a.position-23) > Math.abs(b.position-23)) ? 1 : -1) : -1 ); 
 
-  return notas
+  return notes
 }
 
+//Declaring the visible note.
+let notaVisible = []
 
 
 //Declaring my drawing function 
-let dibuja = function() { //Function to hide and show notes
+let dibuja = function(notes) { //Function to hide and show notes
 
-    algo();
-   let randomElement = Math.floor(Math.random() * 10); //My current algorithm
+  algo(notes);
+
 
      //If there is an element in array, hide previous DOM element and erase first object array.
     if (notaVisible.length > 0) { 
@@ -153,7 +141,7 @@ let dibuja = function() { //Function to hide and show notes
     }
 
     //Introduce new note objet in array and display his DOM element.
-    notaVisible.push(notas[randomElement]); 
+    notaVisible.push(mySong[Math.floor(Math.random() * 10)]); 
     notaVisible[notaVisible.length - 1].elem.style.display = "block"; 
     activeNote = notaVisible[0]
     
@@ -172,25 +160,9 @@ let dibuja = function() { //Function to hide and show notes
             bass.style.display = "none";
 
     }
-console.table(notas)
+//console.table(notes)
     return activeNote
 };
-
-
-
-//Function to check wether the right button has been clicked. 
-let checkTest = function(event) {
-    butonToString = String(event.target.getAttribute('id'))
-
-
-    if (activeNote.note.includes(butonToString)) {
-        let pushTime = storeTime()
-        activeNote.timer = pushTime
-        if(activeNote.timer < 2 ) {activeNote.timer +=100} 
-        dibuja();
-       confeti();
-    }
-}
 
 //When I click the button, the timestamp need to be stored. I could have an array of time stamps that are recorded. The first timestamp is the time when the button is clicked. 
 time = []
@@ -208,21 +180,45 @@ let storeTime = function() {
 }
 
 
-
-
-   
-
-
+//Function to check wether the right button has been clicked. 
+let checkTest = function(event, notes = mySong) {
+    butonToString = String(event.target.getAttribute('id'))
 
 
 
-c.addEventListener('click', checkTest);
-d.addEventListener('click', checkTest);
-e.addEventListener('click', checkTest);
-f.addEventListener('click', checkTest);
-g.addEventListener('click', checkTest);
-a.addEventListener('click', checkTest);
-b.addEventListener('click', checkTest);
-boton.addEventListener('click', dibuja);
+    if (activeNote.note.includes(butonToString)) {
+        let pushTime = storeTime()
+        activeNote.timer = pushTime
+        if(activeNote.timer < 2 ) {activeNote.timer +=100} 
+        dibuja(notes);
+    }
+}
+
+
+
+
+//Adding event listeners to Button notes.
+const flatNotes = [c,d,e,f,g,a,b]
+
+flatNotes.forEach(item => {
+  item.addEventListener('click',checkTest)
+});
+
+
+// When pressing "Start" , check for wether there is a saved song, otherwise play default song.
+//Check if song is saved...
+
+boton.addEventListener('click', function () {
+  const retrievedSong = JSON.parse(localStorage.getItem('savedSong'));
+  if (typeof variable !== 'undefined') {
+    console.log(retrievedSong)
+    initialize(retrievedSong)
+  }
+  else {
+    initialize(mySong)
+    console.log("didnt work mate")
+    console.table(retrievedSong)
+  }
+});
 
 
